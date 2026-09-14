b8a12f5388eb57eefdb768da047809bbe91152cf
rayka999 <raykalgarcia07@gmail.com>
2026-09-14 08:11:40 -0300

    atividade signal

---
+++ loja-frontend/src/app/carrinho-service.ts
@@ -0,0 +1,41 @@
+import { Service, signal } from '@angular/core';
+import { Item } from './carrinho.model';
+
+@Service()
+export class CarrinhoService {
+    readonly itens=signal<Item[]>([])
+
+    addItem(item:Item){
+        this.itens.update(currentItens=> [...currentItens,item])
+    }
+
+    removerItem(idItem:number){
+        this.itens.update(currentItens=>
+            currentItens.filter(item=> item.id !== idItem)
+        )
+    }
+
+    AumQtdItem(idItem: number,qtd:number) {
+        this.itens.update(currentItens => 
+            currentItens.map(item =>
+                item.id === idItem ? { ...item, quantidade: item.quantidade + qtd } : item
+            )
+        );
+    }
+
+    DimQtdItem(idItem:number,qtd:number){
+        this.itens.update(currentItens => 
+            currentItens.map(item =>
+                item.id === idItem ? { ...item, quantidade: item.quantidade - qtd } : item
+            )
+        );
+    }
+
+    obterTotalCompra(){
+        let total=0;
+        this.itens().forEach(item => {
+            total+=(item.produto.preco*item.quantidade)
+        });
+        return total.toFixed(2);
+    }
+}
---
+++ loja-frontend/src/app/carrinho.model.ts
@@ -0,0 +1,7 @@
+import { Produto } from "../produto.model"
+
+export type Item={
+    id:number,
+    produto:Produto
+    quantidade:number
+}
